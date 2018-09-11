4aff171fd42ff7a601e18b37048926889f68e8f3
zhangshun <zhangshun@startimes.com.cn>
2018-09-11 16:41:11 +0800

    添加写文件到sd卡根路径文件的功能

--- easyComLibrary/src/main/java/com/zs/easy/common/utils/LogUtil.java
+++ easyComLibrary/src/main/java/com/zs/easy/common/utils/LogUtil.java
@@ -16,137 +16,138 @@ import java.util.Date;
 
 
 public class LogUtil {
-	private static SimpleDateFormat myLogSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 日志的输出格
-	private static SimpleDateFormat logfile = new SimpleDateFormat("yyyy-MM-dd");// 日志文件格式
-	private static String MYLOGFILEName = "log.txt";// 本类输出的日志文件名称
+    private static SimpleDateFormat myLogSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");// 日志的输出格
+    private static SimpleDateFormat logfile = new SimpleDateFormat("yyyy-MM-dd");// 日志文件格式
+    private static String MYLOGFILEName = "log.txt";// 本类输出的日志文件名称
 
-	private static boolean logable;
-	private static int logLevel;
+    private static boolean logable;
+    private static int logLevel;
 
-	public static void init(boolean isPrintLog){
-		/**
-		 * 设置是否打印日志和日志等级
-		 */
-		setLogable(isPrintLog);
-		setLogLevel(LogPriority.VERBOSE);
-	}
+    public static void init(boolean isPrintLog) {
+        /**
+         * 设置是否打印日志和日志等级
+         */
+        setLogable(isPrintLog);
+        setLogLevel(LogPriority.VERBOSE);
+    }
 
-	public static enum LogPriority {
-		VERBOSE, DEBUG, INFO, WARN, ERROR, ASSERT
-	}
-	
-	public static void d(String tag,String msg){
-		log(LogPriority.DEBUG, tag, msg, null);
-		writeLogToText(msg);
-	}
-	
-	public static void d(String tag,String msg,Throwable tr){
-		log(LogPriority.DEBUG, tag, msg, tr);
-		writeLogToText(msg + tr.toString());
-	}
-	
-	public static void e(String tag,String msg){
-		log(LogPriority.ERROR, tag, msg, null);
-		writeLogToText(msg);
-	}
+    public static enum LogPriority {
+        VERBOSE, DEBUG, INFO, WARN, ERROR, ASSERT
+    }
 
-	public static void e(String msg){
-		log(LogPriority.ERROR, EasyConstants.TAG, msg, null);
-		writeLogToText(msg);
-	}
-	
-	public static void e(String tag,String msg,Throwable tr){
-		log(LogPriority.ERROR, tag, msg, tr);
-		Writer writer = new StringWriter();
-		PrintWriter printWriter = new PrintWriter(writer);
-		tr.printStackTrace(printWriter);
-		Throwable cause = tr.getCause();
-		while (cause != null) {
-			cause.printStackTrace(printWriter);
-			cause = cause.getCause();
-		}
-		printWriter.close();
-		String result = writer.toString();
-		writeLogToText(msg + "\n"+ result);
-	}
-	
-	public static void i(String tag,String msg){
-		log(LogPriority.INFO, tag, msg, null);
-		writeLogToText(msg);
-	}
+    public static void d(String tag, String msg) {
+        log(LogPriority.DEBUG, tag, msg, null);
+        writeLogToText(msg);
+    }
 
-	public static void i(String msg){
-		log(LogPriority.INFO, EasyConstants.TAG, msg, null);
-		writeLogToText(msg);
-	}
-	
-	public static void i(String tag,String msg,Throwable tr){
-		log(LogPriority.INFO, tag, msg, tr);
-		writeLogToText(msg +  "\n" + tr.toString());
-	}
-	
-	public static void v(String tag,String msg){
-		log(LogPriority.VERBOSE, tag, msg, null);
-		writeLogToText(msg);
-	}
-	public static void v(String tag,String msg,Throwable tr){
-		log(LogPriority.VERBOSE, tag, msg, tr);
-		writeLogToText(msg +  "\n" + tr.toString());
-	}
-	
-	public static void w(String tag,String msg){
-		log(LogPriority.WARN, tag, msg, null);
-		writeLogToText(msg);
-	}
-	
-	public static void w(String tag,String msg,Throwable tr){
-		log(LogPriority.WARN, tag, msg, tr);
-		writeLogToText(msg +  "\n" + tr.toString());
-	}
-	
-	
-	/**
-	 * 获取是否可打印日志
-	 * 
-	 * @return
-	 */
-	public static boolean isLogable() {
-		return logable;
-	}
+    public static void d(String tag, String msg, Throwable tr) {
+        log(LogPriority.DEBUG, tag, msg, tr);
+        writeLogToText(msg + tr.toString());
+    }
 
-	/**
-	 * 设置是否打印日志
-	 * 
-	 * @param logable
-	 */
-	public static void setLogable(boolean logable) {
-		LogUtil.logable = logable;
-	}
+    public static void e(String tag, String msg) {
+        log(LogPriority.ERROR, tag, msg, null);
+        writeLogToText(msg);
+    }
 
-	/**
-	 * 设置日志等级
-	 * 
-	 * @param level
-	 */
-	public static void setLogLevel(LogPriority level) {
-		LogUtil.logLevel = level.ordinal();
-	}
+    public static void e(String msg) {
+        log(LogPriority.ERROR, EasyConstants.TAG, msg, null);
+        writeLogToText(msg);
+    }
 
-	
-	private static void log(LogPriority priority,String tag,String msg,Throwable tr){
-		int level = priority.ordinal();
-		if (logable && logLevel <= level) {
-			if(tr != null){
-				msg = msg + "\n" + Log.getStackTraceString(tr);
-			}
-			Log.println(level + 2, tag, msg);
-		} 
-	}
+    public static void e(String tag, String msg, Throwable tr) {
+        log(LogPriority.ERROR, tag, msg, tr);
+        Writer writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        tr.printStackTrace(printWriter);
+        Throwable cause = tr.getCause();
+        while (cause != null) {
+            cause.printStackTrace(printWriter);
+            cause = cause.getCause();
+        }
+        printWriter.close();
+        String result = writer.toString();
+        writeLogToText(msg + "\n" + result);
+    }
 
-	//将日志写到文件中
-	public static void writeLogToText(String log){
-		try {
-			/*if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+    public static void i(String tag, String msg) {
+        log(LogPriority.INFO, tag, msg, null);
+        writeLogToText(msg);
+    }
+
+    public static void i(String msg) {
+        log(LogPriority.INFO, EasyConstants.TAG, msg, null);
+        writeLogToText(msg);
+    }
+
+    public static void i(String tag, String msg, Throwable tr) {
+        log(LogPriority.INFO, tag, msg, tr);
+        writeLogToText(msg + "\n" + tr.toString());
+    }
+
+    public static void v(String tag, String msg) {
+        log(LogPriority.VERBOSE, tag, msg, null);
+        writeLogToText(msg);
+    }
+
+    public static void v(String tag, String msg, Throwable tr) {
+        log(LogPriority.VERBOSE, tag, msg, tr);
+        writeLogToText(msg + "\n" + tr.toString());
+    }
+
+    public static void w(String tag, String msg) {
+        log(LogPriority.WARN, tag, msg, null);
+        writeLogToText(msg);
+    }
+
+    public static void w(String tag, String msg, Throwable tr) {
+        log(LogPriority.WARN, tag, msg, tr);
+        writeLogToText(msg + "\n" + tr.toString());
+    }
+
+
+    /**
+     * 获取是否可打印日志
+     *
+     * @return
+     */
+    public static boolean isLogable() {
+        return logable;
+    }
+
+    /**
+     * 设置是否打印日志
+     *
+     * @param logable
+     */
+    public static void setLogable(boolean logable) {
+        LogUtil.logable = logable;
+    }
+
+    /**
+     * 设置日志等级
+     *
+     * @param level
+     */
+    public static void setLogLevel(LogPriority level) {
+        LogUtil.logLevel = level.ordinal();
+    }
+
+
+    private static void log(LogPriority priority, String tag, String msg, Throwable tr) {
+        int level = priority.ordinal();
+        if (logable && logLevel <= level) {
+            if (tr != null) {
+                msg = msg + "\n" + Log.getStackTraceString(tr);
+            }
+            Log.println(level + 2, tag, msg);
+        }
+    }
+
+    //将日志写到文件中
+    public static void writeLogToText(String log) {
+        try {
+            /*if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
 				File dir = new File(StarConstants.FILE_PATH + "log");
 				if (!dir.exists()) {
 					dir.mkdirs();
@@ -158,51 +159,80 @@ public class LogUtil {
 				fos.write(sb.toString().getBytes());
 				fos.close();
 			}*/
-		} catch (Exception e) {
-			LogUtil.e(EasyConstants.TAG, "an error occured while writing log file...");
-		}
-	}
+        } catch (Exception e) {
+            LogUtil.e(EasyConstants.TAG, "an error occured while writing log file...");
+        }
+    }
 
-	/**
-	 * 打开日志文件并写入日志
-	 *
-	 * @return
-	 **/
-	private static void writeLogtoFile(String tag, String text) {// 新建或打印到日志文件
-		Log.i(tag, text);
-		Date nowtime = new Date();
-		String needWriteFiel = logfile.format(nowtime);
-		String needWriteMessage = myLogSdf.format(nowtime) + "    " + tag + "    " + text;
+    /**
+     * 打开日志文件并写入日志
+     *
+     * @return
+     **/
+    private static void writeLogtoFile(String tag, String text) {// 新建或打印到日志文件
+        Log.i(tag, text);
+        Date nowtime = new Date();
+        String needWriteFiel = logfile.format(nowtime);
+        String needWriteMessage = myLogSdf.format(nowtime) + "    " + tag + "    " + text;
 
-		// 取得日志存放目录
-		String path = getLogPath();
-		if (path != null && !"".equals(path)) {
-			try {
-				// 打开文件
-				File file = new File(path + File.separator + needWriteFiel + MYLOGFILEName);
-				FileWriter filerWriter = new FileWriter(file, true);// 后面这个参数代表是不是要接上文件中原来的数据，不进行覆盖
-				BufferedWriter bufWriter = new BufferedWriter(filerWriter);
-				bufWriter.write(needWriteMessage);
-				bufWriter.newLine();
-				bufWriter.close();
-				filerWriter.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        // 取得日志存放目录
+        String path = getLogPath();
+        if (path != null && !"".equals(path)) {
+            try {
+                // 打开文件
+                File file = new File(path + File.separator + needWriteFiel + MYLOGFILEName);
+                FileWriter filerWriter = new FileWriter(file, true);// 后面这个参数代表是不是要接上文件中原来的数据，不进行覆盖
+                BufferedWriter bufWriter = new BufferedWriter(filerWriter);
+                bufWriter.write(needWriteMessage);
+                bufWriter.newLine();
+                bufWriter.close();
+                filerWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	private static String getLogPath() {
-		String path = "";
-		// 获取扩展SD卡设备状太
-		String sDStateString = android.os.Environment.getExternalStorageState();
+    /**
+     * 打开日志文件并写入日志 自定义文件名
+     *
+     * @return
+     **/
+    private static void writeLogtoFile(String tag, String text, String fileName) {// 新建或打印到日志文件
+        Log.i(tag, text);
+        Date nowtime = new Date();
+        String needWriteFiel = logfile.format(nowtime);
+        String needWriteMessage = myLogSdf.format(nowtime) + "    " + tag + "    " + text;
 
-		// 拥有可读可写权限
-		if (sDStateString.equals(android.os.Environment.MEDIA_MOUNTED)) {
-			// 获取扩展存储设备的文件目录
-			File SDFile = android.os.Environment.getExternalStorageDirectory();
-			path = SDFile.getAbsolutePath();
-		}
-		return path;
-	}
+        // 取得日志存放目录
+        String path = getLogPath();
+        if (path != null && !"".equals(path)) {
+            try {
+                // 打开文件
+                File file = new File(path + File.separator + fileName);
+                FileWriter filerWriter = new FileWriter(file, true);// 后面这个参数代表是不是要接上文件中原来的数据，不进行覆盖
+                BufferedWriter bufWriter = new BufferedWriter(filerWriter);
+                bufWriter.write(needWriteMessage);
+                bufWriter.newLine();
+                bufWriter.close();
+                filerWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static String getLogPath() {
+        String path = "";
+        // 获取扩展SD卡设备状太
+        String sDStateString = android.os.Environment.getExternalStorageState();
+
+        // 拥有可读可写权限
+        if (sDStateString.equals(android.os.Environment.MEDIA_MOUNTED)) {
+            // 获取扩展存储设备的文件目录
+            File SDFile = android.os.Environment.getExternalStorageDirectory();
+            path = SDFile.getAbsolutePath();
+        }
+        return path;
+    }
 }
