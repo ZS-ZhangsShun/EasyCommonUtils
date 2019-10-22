94bff620920c77742238fbc15e288e503384eec4
18763552493@163.com <18763552493@163.com>
2019-10-22 22:17:10 +0800

    ActivityStyleUtil 改为 StyleUtil，因为Dialog也可以使用这个工具类，另外 里面设置标题栏浸润样式的方法参数由原来的Activity activity 改为 Window window

---
+++ easyComLibrary/src/main/java/com/zs/easy/common/utils/StyleUtil.java
@@ -0,0 +1,35 @@
+package com.zs.easy.common.utils;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by zhangshun on 2018/1/27.
+ */
+
+public class StyleUtil {
+
+    /**
+     * 设置状态栏透明的风格
+     */
+    public static void setStatusTransparentStyle(Window window, boolean isTextBlack) {
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //android 6.0及以上支持改变状态栏字体颜色
+            if (isTextBlack) {
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            } else {
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            }
+
+            //android 5.0及以上支持改变状态栏为透明状态
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+}
